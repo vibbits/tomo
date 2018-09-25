76ef9b1736a22df543c9d95a2903a36d7a686363
Frank Vernaillen <frank.vernaillen@ugent.be>
2018-09-25 11:58:45 +0200

    Removed debug print. Also fixed incorrect Dialog/Frame inheritance. Note that a wx.Dialog cannot have a status bar.

--- experiments/navcanvasdialog.py
+++ experiments/navcanvasdialog.py
@@ -7,10 +7,10 @@ from wx.lib.floatcanvas import NavCanvas, FloatCanvas
 
 filename = r'F:\Secom\sergio_x20_LM_objective_overview_image\bisstitched-0.tif'
 
-class DrawDialog(wx.Frame):
+class DrawFrame(wx.Frame):
     navpanel = None
     def __init__(self, *args, **kwargs):
-        wx.Dialog.__init__(self, *args, **kwargs)
+        wx.Frame.__init__(self, *args, **kwargs)
         self.CreateStatusBar() 
 
         self.navpanel = NavPanel(self)        
@@ -24,9 +24,9 @@ class DrawDialog(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnPolygonButtonClick, polybutton)
 
         MainSizer = wx.BoxSizer(wx.VERTICAL)
-        MainSizer.Add(imagebutton, 0, wx.CENTER | wx.EXPAND | wx.ALL, 1)    # Add(item, proportion, flags, border)
-        MainSizer.Add(polybutton, 0, wx.CENTER | wx.EXPAND | wx.ALL, 1)    # Add(item, proportion, flags, border)
-        MainSizer.Add(self.navpanel, 4, wx.EXPAND)
+        MainSizer.Add(imagebutton, 0, wx.CENTER | wx.EXPAND | wx.ALL, 1)  # Add(item, proportion, flags, border)
+        MainSizer.Add(polybutton, 0, wx.CENTER | wx.EXPAND | wx.ALL, 1)   # wx.ALL=add border along all 4 edges
+        MainSizer.Add(self.navpanel, 1, wx.EXPAND)  # note: proportion=1 here is crucial, 0 will not work
         self.SetSizer(MainSizer)
 
     def OnImageButtonClick(self, event):
@@ -38,7 +38,6 @@ class DrawDialog(wx.Frame):
         self.navpanel.Canvas.ZoomToBB()
 
     def OnMove(self, event): 
-        print(event.Coords)
         self.SetStatusText("%i, %i" % (event.Coords[0], -event.Coords[1]))   # flip y so we have the y-axis pointing down and (0,0)= top left corner of the image
 
 class NavPanel(NavCanvas.NavCanvas):
@@ -59,6 +58,6 @@ class NavPanel(NavCanvas.NavCanvas):
         self.Canvas.AddObject(img) 
 
 app = wx.App(False)
-dlg = DrawDialog(None, title="FloatCanvas Dialog Demo", size=(700, 700))
+dlg = DrawFrame(None, title="NavCanvas Demo", size=(700, 700))
 dlg.Show()
 app.MainLoop()
