207d28ae87bcef8dc89b56a6e30243d0a51f70ff
Frank Vernaillen <frank.vernaillen@ugent.be>
2018-10-01 15:39:31 +0200

    LM autofocus on/off and max z change are now user options

--- src/lm_acquisition_dialog.py
+++ src/lm_acquisition_dialog.py
@@ -11,7 +11,7 @@ class LMAcquisitionDialog(wx.Dialog):
     # UI elements
     _lm_images_output_folder_edit = None
     _prefix_edit = None
-    _lm_acquisition_delay_text = None
+    _lm_acquisition_delay_edit = None
     _sift_input_folder_edit = None
     _sift_output_folder_edit = None
     _sift_pixel_size_edit = None
@@ -19,6 +19,9 @@ class LMAcquisitionDialog(wx.Dialog):
     _sift_output_folder_button = None
     _lm_images_output_folder_button = None
     _acquire_button = None
+    _lm_max_autofocus_change_label = None
+    _lm_max_autofocus_change_edit = None
+    _lm_do_autofocus_checkbox = None
 
     def __init__(self, model, parent, ID, title, size = wx.DefaultSize, pos = wx.DefaultPosition, style = wx.DEFAULT_DIALOG_STYLE):
         wx.Dialog.__init__(self, parent, ID, title, pos, size, style)
@@ -36,12 +39,22 @@ class LMAcquisitionDialog(wx.Dialog):
         self._prefix_edit = wx.TextCtrl(self, wx.ID_ANY, self._model.lm_images_prefix, size = (w, -1))
 
         lm_acquisition_delay_label = wx.StaticText(self, wx.ID_ANY, "Acquisition Delay (sec):")
-        self._lm_acquisition_delay_text = wx.TextCtrl(self, wx.ID_ANY, str(self._model.delay_between_LM_image_acquisition_secs), size = (50, -1))
+        self._lm_acquisition_delay_edit = wx.TextCtrl(self, wx.ID_ANY, str(self._model.delay_between_LM_image_acquisition_secs), size = (50, -1))
 
         lm_sizer = wx.BoxSizer(wx.HORIZONTAL)
         lm_sizer.Add(self._lm_images_output_folder_edit, flag = wx.ALIGN_CENTER_VERTICAL)
         lm_sizer.AddSpacer(8)
         lm_sizer.Add(self._lm_images_output_folder_button, flag = wx.ALIGN_CENTER_VERTICAL)
+
+        empty_label = wx.StaticText(self, wx.ID_ANY, "")
+
+        self._lm_do_autofocus_checkbox = wx.CheckBox(self, wx.ID_ANY, "Perform Autofocus")
+        self._lm_do_autofocus_checkbox.SetValue(self._model.lm_do_autofocus)
+
+        self._lm_max_autofocus_change_label = wx.StaticText(self, wx.ID_ANY, "Max. Autofocus Change (micrometer):")
+        self._lm_max_autofocus_change_edit = wx.TextCtrl(self, wx.ID_ANY, str(self._model.lm_max_autofocus_change_microns), size = (50, -1))
+
+        self._enable_autofocus_edit_field(self._model.lm_do_autofocus)
 
         #
         sift_input_folder_label = wx.StaticText(self, wx.ID_ANY, "Input Folder:")
@@ -72,7 +85,11 @@ class LMAcquisitionDialog(wx.Dialog):
         lm_fgs.Add(prefix_label, flag = wx.LEFT | wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
         lm_fgs.Add(self._prefix_edit, flag = wx.RIGHT | wx.ALIGN_CENTER_VERTICAL)
         lm_fgs.Add(lm_acquisition_delay_label, flag = wx.LEFT | wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
-        lm_fgs.Add(self._lm_acquisition_delay_text, flag = wx.RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        lm_fgs.Add(self._lm_acquisition_delay_edit, flag =wx.RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        lm_fgs.Add(empty_label, flag = wx.LEFT | wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        lm_fgs.Add(self._lm_do_autofocus_checkbox, flag = wx.RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        lm_fgs.Add(self._lm_max_autofocus_change_label, flag = wx.LEFT | wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        lm_fgs.Add(self._lm_max_autofocus_change_edit, flag =wx.RIGHT | wx.ALIGN_CENTER_VERTICAL)
 
         lm_box = wx.StaticBox(self, -1, 'LM Image Acquisition')
         lm_sizer = wx.StaticBoxSizer(lm_box, wx.VERTICAL)
@@ -91,14 +108,16 @@ class LMAcquisitionDialog(wx.Dialog):
         sift_sizer = wx.StaticBoxSizer(sift_box, wx.VERTICAL)
         sift_sizer.Add(sift_fgs, 0, wx.ALL|wx.CENTER, 10)
 
-        self._acquire_button = wx.Button(self, wx.ID_ANY, "Acquire LM Images!")
+        self._acquire_button = wx.Button(self, wx.ID_ANY, "Acquire LM Images")
 
         self.Bind(wx.EVT_TEXT, self._on_lm_images_output_folder_change, self._lm_images_output_folder_edit)
         self.Bind(wx.EVT_TEXT, self._on_prefix_change, self._prefix_edit)
-        self.Bind(wx.EVT_TEXT, self._on_delay_change, self._lm_acquisition_delay_text)
+        self.Bind(wx.EVT_TEXT, self._on_delay_change, self._lm_acquisition_delay_edit)
         self.Bind(wx.EVT_TEXT, self._on_sift_input_folder_change, self._sift_input_folder_edit)
         self.Bind(wx.EVT_TEXT, self._on_sift_output_folder_change, self._sift_output_folder_edit)
         self.Bind(wx.EVT_TEXT, self._on_sift_pixel_size_change, self._sift_pixel_size_edit)
+        self.Bind(wx.EVT_CHECKBOX, self._on_lm_do_autofocus_change, self._lm_do_autofocus_checkbox)
+        self.Bind(wx.EVT_TEXT, self._on_lm_max_autofocus_change, self._lm_max_autofocus_change_edit)
         self.Bind(wx.EVT_BUTTON, self._on_lm_output_folder_browse_button_click, self._lm_images_output_folder_button)
         self.Bind(wx.EVT_BUTTON, self._on_sift_input_folder_browse_button_click, self._sift_input_folder_button)
         self.Bind(wx.EVT_BUTTON, self._on_sift_output_folder_browse_button_click, self._sift_output_folder_button)
@@ -112,7 +131,11 @@ class LMAcquisitionDialog(wx.Dialog):
         self.SetSizer(contents)
         contents.Fit(self)
 
-    # TODO: try to generalize/unify the 3 functions below
+    def _enable_autofocus_edit_field(self, enable):
+        self._lm_max_autofocus_change_label.Enable(enable)
+        self._lm_max_autofocus_change_edit.Enable(enable)
+
+    # TODO: try to generalize/unify the 3 functions below (and similar ones in other ui source files)
 
     def _on_lm_output_folder_browse_button_click(self, event):
         defaultPath = self._model.lm_images_output_folder
@@ -156,8 +179,17 @@ class LMAcquisitionDialog(wx.Dialog):
         print('lm_images_prefix={}'.format(self._model.lm_images_prefix))
 
     def _on_delay_change(self, event):
-        self._model.delay_between_LM_image_acquisition_secs = float(self._lm_acquisition_delay_text.GetValue())
+        self._model.delay_between_LM_image_acquisition_secs = float(self._lm_acquisition_delay_edit.GetValue())
         print('delay_between_LM_image_acquisition_secs={}'.format(self._model.delay_between_LM_image_acquisition_secs))
+
+    def _on_lm_do_autofocus_change(self, event):
+        self._model.lm_do_autofocus = self._lm_do_autofocus_checkbox.IsChecked()
+        print('lm_do_autofocus={}'.format(self._model.lm_do_autofocus))
+        self._enable_autofocus_edit_field(self._model.lm_do_autofocus)
+
+    def _on_lm_max_autofocus_change(self, event):
+        self._model.lm_max_autofocus_change_microns = float(self._lm_max_autofocus_change_edit.GetValue())
+        print('lm_max_autofocus_change_microns={}'.format(self._model.lm_max_autofocus_change_microns))
 
     def _on_sift_input_folder_change(self, event):
         self._model.sift_input_folder = self._sift_input_folder_edit.GetValue()
--- src/model.py
+++ src/model.py
@@ -15,6 +15,8 @@ class TomoModel:
     _KEY_ORIGINAL_POI_Y = 'original_poi_y'
     _KEY_LM_ACQUISITION_DELAY = 'lm_acquisition_delay'
     _KEY_EM_ACQUISITION_DELAY = 'em_acquisition_delay'
+    _KEY_LM_DO_AUTOFOCUS = 'lm_do_autofocus'
+    _KEY_LM_MAX_AUTOFOCUS_CHANGE_MICRONS = 'lm_max_autofocus_change_microns'
     _KEY_OVERVIEW_IMAGE_MM_PER_PIXEL = 'overview_image_mm_per_pixel'
     _KEY_SIFT_IMAGES_MM_PER_PIXEL = 'sift_images_mm_per_pixel'
     _KEY_FIJI_PATH = 'fiji_path'
@@ -35,6 +37,8 @@ class TomoModel:
     ribbons_mask_path = None
     lm_images_output_folder = None
     em_images_output_folder = None
+    lm_do_autofocus = False
+    lm_max_autofocus_change_microns = 0.0
     original_point_of_interest = np.array([0, 0])
     delay_between_LM_image_acquisition_secs = 0.0  # time in seconds to pause between successive microscope commands to acquire an LM image (maybe 1 or 2 secs in reality)
     delay_between_EM_image_acquisition_secs = 0.0  # time in seconds to pause between successive microscope commands to acquire an EM image (maybe 1 or 2 secs in reality)
@@ -79,6 +83,8 @@ class TomoModel:
         self.sift_output_folder                      = self._config.Read(TomoModel._KEY_SIFT_OUTPUT_FOLDER, r'/home/secom/development/tomo/data/output/LM')
         self.sift_images_mm_per_pixel                = self._config.ReadFloat(TomoModel._KEY_SIFT_IMAGES_MM_PER_PIXEL, 1000.0)  # just a random value, probably not typical
         self.template_slice_path                     = self._config.Read(TomoModel._KEY_TEMPLATE_SLICE_PATH, r'/home/secom/some/folder/template_slice_contour.json')
+        self.lm_do_autofocus                         = self._config.ReadBool(TomoModel._KEY_LM_DO_AUTOFOCUS, True)
+        self.lm_max_autofocus_change_microns         = self._config.ReadFloat(TomoModel._KEY_LM_MAX_AUTOFOCUS_CHANGE_MICRONS, 5.0)
 
     def write_parameters(self):
         self._config.Write(TomoModel._KEY_OVERVIEW_IMAGE_PATH, self.overview_image_path)
@@ -100,6 +106,8 @@ class TomoModel:
         self._config.Write(TomoModel._KEY_SIFT_OUTPUT_FOLDER, self.sift_output_folder)
         self._config.WriteFloat(TomoModel._KEY_SIFT_IMAGES_MM_PER_PIXEL, self.sift_images_mm_per_pixel)
         self._config.Write(TomoModel._KEY_TEMPLATE_SLICE_PATH, self.template_slice_path)
+        self._config.WriteBool(TomoModel._KEY_LM_DO_AUTOFOCUS, self.lm_do_autofocus)
+        self._config.WriteFloat(TomoModel._KEY_LM_MAX_AUTOFOCUS_CHANGE_MICRONS, self.lm_max_autofocus_change_microns)
         self._config.Flush()
 
     # # Input parameters # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
