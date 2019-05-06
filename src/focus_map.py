dff397e647fa1fa23dfb6d5517994ae412fff2a8
Frank Vernaillen <frank.vernaillen@ugent.be>
2019-05-06 11:31:37 +0200

    Removed incorrect or obsolete comments. Improved documentation.

--- src/focus_map.py
+++ src/focus_map.py
@@ -56,3 +56,47 @@ class FocusMap:
 
         print('Building focus sample grid using {}'.format(self._interpolator))
         self._interpolator.prepare(self._xmin, self._xmax, self._ymin, self._ymax, self._step)
+
+    def __str__(self):
+        s = 'Focus map area:\nx={} to {}\ny={} to {}\nstep size={}\n\n'.format(self._xmin, self._xmax, self._ymin, self._ymax, self._step)
+        s = s + 'Focus samples:\n'
+        for x, y, z in self._focus_positions:
+            s = s + 'x={} y={} z={}\n'.format(x, y, z)
+        return s
+
+    @classmethod
+    def load(cls, filename):
+        # Construct a FocusMap object from the text file with the given filename.
+        # The file's contents look like this:
+        #
+        #    xmin xmax ymin ymax stepsize
+        #    x1 y1 z1
+        #    ...
+        #    xn yn zn
+        #
+        # Lines starting with (optional) whitespace followed by a hash sign are ignored (useful for comments).
+        # Lines with only whitespace are ignored as well.
+
+        # Read the focus map text file
+        with open(filename) as f:
+            lines = f.readlines()
+
+        # Parse the focus map text lines
+
+        # Discard empty lines and comment lines. Comment lines start with a hash sign.
+        lines = [line for line in lines if not (line.lstrip().startswith('#') or len(line.strip()) == 0)]
+
+        # Read extent and stepsize
+        xmin, xmax, ymin, ymax, step = [float(val) for val in lines[0].split()]
+
+        # Read focus values
+        focus_data = []
+        for line in lines[1:]:
+            x, y, z = [float(val) for val in line.split()]
+            focus_data.append((x, y, z))
+
+        # Construct the focus map
+        focusmp = cls(xmin, xmax, ymin, ymax, step)
+        for focus in focus_data:
+            focusmp.add_user_defined_focus_position((focus[0], focus[1]), focus[2])
+        return focusmp
