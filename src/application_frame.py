0082465cd5c4a8097ee75bbf428f3229a416eebe
Frank Vernaillen <frank.vernaillen@ugent.be>
2020-04-17 07:33:19 +0200

    Fixed crash due to missing experimental menu

--- src/application_frame.py
+++ src/application_frame.py
@@ -197,9 +197,14 @@ class ApplicationFrame(wx.Frame):
         self._contour_finder_item = segmentation_menu.Append(wx.NewId(), "Find slice contours...")  # an active contours (style) contour fitting prototype
         self._contour_finder_item.Enable(False)
 
+        # Enable/disable unfinished/unstable experiments. Disabled by default.
+        experimenting = False
+
         experimental_menu = wx.Menu()
         self._segment_ribbons_item = experimental_menu.Append(wx.NewId(), "Segment Ribbons...")
+        self._segment_ribbons_item.Enable(experimenting)
         self._save_screenshot_item = experimental_menu.Append(wx.NewId(), "Save Screenshot...")
+        self._save_screenshot_item.Enable(experimenting)
 
         view_menu = wx.Menu()
         self._show_slice_numbers_item = view_menu.Append(wx.NewId(), "Show slice numbers", kind=wx.ITEM_CHECK)
@@ -208,15 +213,11 @@ class ApplicationFrame(wx.Frame):
         help_menu = wx.Menu()
         self._about_item = help_menu.Append(wx.NewId(), "About")
 
-        # Enable/disable unfinished/unstable experiments. Disabled by default.
-        experimenting = False
-
         menu_bar.Append(file_menu, "&File")
         menu_bar.Append(edit_menu, "&Edit")
         menu_bar.Append(microscope_menu, "&Microscope")
         menu_bar.Append(segmentation_menu, "&Segmentation")
-        if experimenting:
-            menu_bar.Append(experimental_menu, "&Experimental")
+        menu_bar.Append(experimental_menu, "&Experimental")
         menu_bar.Append(view_menu, "&View")
         menu_bar.Append(help_menu, "&Help")
 
